d664f430b1d4c2a5d1d213f6ae3a28bbac141075
Mohd.Adil <Mohd.Adil@host.docker.internal>
2020-02-25 15:10:00 +0530

    add project

---
+++ src/main/java/com/microservice/gateway/SpringGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayApplication.class, args);
+		
+	}
+
+}
